341bccdcd728f5751a4a3ea114e3903a2d03240d
Neryed120819 <eduardo11.agr@gmail.com>
2019-09-12 19:17:46 -0500

    Punteros

---
+++ mayor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+	int arreglo[10],*puntero;
+	puntero=&arreglo[0];
+	int mayor=INT_MIN;
+	
+	for(int i=0; i<=9; i++){
+	cout<<"Ingrese los valores"<<i+1<<endl;
+	cin>>arreglo[i];
+	
+}
+
+	for(int *puntero=&arreglo[0]; puntero<=&arreglo[9]; puntero++){
+		if (*puntero>mayor) mayor=*puntero;
+
+	}
+	
+	cout<<"El valor mayor es: "<<mayor<<endl;
+	return 0;
+}
